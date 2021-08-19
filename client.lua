3461f6fc8427340b0b4633259026c3b2c314f715
Obtaizen <joshuamills9999@gmail.com>
2021-08-20 00:29:44 +0100

    [qb-signrobbery] Added Pedestrian Sign

--- client.lua
+++ client.lua
@@ -27,6 +27,26 @@ AddEventHandler("stopsign:client:Target", function()
     end)
 end)
 
+RegisterNetEvent("walkingmansign:client:Target")
+AddEventHandler("walkingmansign:client:Target", function()
+    local coords = GetEntityCoords(PlayerPedId())
+    local obj = GetClosestObjectOfType(coords.x, coords.y, coords.z, 10.0, 1502931467, false, false, false)
+    SetEntityAsMissionEntity(obj, true, true)
+    loadAnimDict("amb@prop_human_bum_bin@base")
+    TaskPlayAnim(PlayerPedId(), "amb@prop_human_bum_bin@base", "base", 8.0, 1.0, -1, 49, 0, 0, 0, 0)
+    QBCore.Functions.Progressbar("robbing_sign", "Stealing Pedestrian Sign..", math.random(5000, 7000), false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, function()
+    end, function()
+        StopAnimTask(PlayerPedId(), "amb@prop_human_bum_bin@base", "base", 1.0)
+        TriggerServerEvent("walkingmansign:server:additem")
+        DeleteEntity(obj)
+    end)
+end)
+
 function loadAnimDict(dict)
     while(not HasAnimDictLoaded(dict)) do
         RequestAnimDict(dict)
--- server.lua
+++ server.lua
@@ -10,3 +10,12 @@ AddEventHandler("stopsign:server:additem", function()
 	xPlayer.Functions.AddItem("stopsign", 1, false)
 	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['stopsign'], "add")
 end)
+
+RegisterNetEvent("walkingmansign:server:additem")
+AddEventHandler("walkingmansign:server:additem", function()
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(tonumber(src))
+
+	xPlayer.Functions.AddItem("walkingmansign", 1, false)
+	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['walkingmansign'], "add")
+end)
